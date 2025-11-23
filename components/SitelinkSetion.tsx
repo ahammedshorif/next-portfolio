b1b35ef1d2ac055b483ac2843c3ccdbdee1735a3
Ahammed Shorif <ahammedshorif20@gmail.com>
2025-11-23 23:49:32 +0600

    Sitelink Section CC

---
+++ components/SitelinkSetion.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SitelinkSetion = () => {
+  return (
+    <div>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+    </div>
+  )
+}
+
+export default SitelinkSetion
